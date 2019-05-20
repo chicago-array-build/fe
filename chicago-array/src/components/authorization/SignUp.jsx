6d31f50da53a21764714e2a0896023bb4de45704
greenhornsr <srollins11279@gmail.com>
2019-05-20 15:44:22 -0700

    built out signup and signin components.  confirmed working.  added axiosWithAuth.js to gitignore. token received

---
+++ chicago-array/src/components/authorization/SignUp.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { signUp } from '../../actions';
+
+class SignUp extends React.Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleChange = (event) => {
+        // console.log(this.state)
+        this.setState({
+            ...this.state,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    signUp = (event) => {
+        event.preventDefault()
+        this.props.signUp(this.state)
+    }
+
+    render(){
+        return (
+            <section>
+                <form onSubmit={this.signUp} action="">
+                    <input required onChange={this.handleChange} name="username" value={this.state.username} type="text" placeholder="Username" />
+                    <input required onChange={this.handleChange} name="password" value={this.state.password} type="password" placeholder="Password" />
+                    <button onClick={this.signUp} type="submit">SignUp</button>
+                </form>
+            </section>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    ...state,
+})
+
+export default connect(
+    mapStateToProps,
+    { signUp }
+)(SignUp)
--- chicago-array/src/reducers/index.jsx
+++ chicago-array/src/reducers/index.jsx
@@ -1,19 +1,35 @@
-import LOGIN_START from '../actions';
+import { SIGNIN_START, SIGNIN_FAILURE, SIGNUP_START, SIGNUP_FAILURE } from '../actions';
 
 const initialState = {
     unknownfornow: [],
     username: '',
     password: '',
+    signingUp: false, 
     loggingIn: false,
 }
 
 
 export default function reducer(state = initialState, action){
     switch(action.type){
-        case LOGIN_START:
+        case SIGNIN_START:
         return {
             ...state,
             loggingIn: true,
+        }
+        case SIGNIN_FAILURE:
+        return {
+            ...state,
+            loggingIn: false,
+        }
+        case SIGNUP_START:
+        return {
+            ...state, 
+            signingUp: true, 
+        }
+        case SIGNUP_FAILURE:
+        return {
+            ...state,
+            signingUp: false,
         }
     default: return state
     }
